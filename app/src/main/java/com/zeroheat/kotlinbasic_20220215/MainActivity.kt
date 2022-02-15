78e5ca8d21e0bf90ca6f3683becd313a82ef9db8
zeroheat0930 <dongjunkorea@naver.com>
2022-02-15 10:40:19 +0900

    토스트 로그 찍어보기

--- app/src/main/java/com/zeroheat/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/zeroheat/kotlinbasic_20220215/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zeroheat.kotlinbasic_20220215
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-btnLog.setOnClickListener{
+        // btnLog가 눌리면 어떤 코드를 실행할지, 할 일을 설정.
+        btnLog.setOnClickListener{
 
-    Log.d("메인화면","로그 버튼 눌림")
+            // 로그를 찍어보는 기능 실행. => btnLog가 클릭될 떄 마다 실행.
+            Log.d("메인화면","로그 버튼 눌림")
+            Log.d("메인화면","추가 로그 찍어보기")
+            Log.e("메인화면","e로 로그 찍기")
 }
+
+        btnToast.setOnClickListener{
+     // 토스트 버튼이 눌리면, Toast를 만들고 (makeText) 보여주기 (show)
+            Toast.makeText(this,"토스트를 띄워봅시다.", Toast.LENGTH_SHORT).show()
+        }
 
 
     }
