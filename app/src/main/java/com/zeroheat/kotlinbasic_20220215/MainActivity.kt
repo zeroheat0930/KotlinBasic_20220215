4573a676d203fdd584a7550ea3f1b97d9f5b4015
zeroheat0930 <dongjunkorea@naver.com>
2022-02-15 10:07:16 +0900

    버튼이 눌리면 > 로그 찍어보기

--- app/src/main/java/com/zeroheat/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/zeroheat/kotlinbasic_20220215/MainActivity.kt
@@ -2,10 +2,19 @@ package com.zeroheat.kotlinbasic_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+btnLog.setOnClickListener{
+
+    Log.d("메인화면","로그 버튼 눌림")
+}
+
+
     }
 }
